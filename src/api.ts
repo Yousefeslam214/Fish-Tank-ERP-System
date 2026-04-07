ad1b80dd77491da7a49959d4cae2a4daa59b3783
fouadkhalied <foukha49@gmail.com>
2026-04-07 17:14:23 +0200

    refactor: update sync flow

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import { getAccessToken } from './services/authSession';
 
+
 export const DEFAULT_API_BASE = 'https://fouadkhaild-asd.hf.space/api/v1';
 
 const getEnvValue = (key: string): string | undefined => {
